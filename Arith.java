c4dafe1dcb03c4a0b3b1d201fe5fb67e06c76343
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-04-29 10:30:28 +0530

    save

---
+++ Arith.java
@@ -0,0 +1,10 @@
+public class Arith {
+    public static void main(String[] args) {
+        int a=5;
+        int b=9;
+        System.out.println("Add = "+(a+b));
+        System.out.println("Sub = "+(a-b));
+        System.out.println("Mul = "+(a*b));
+        System.out.println("Div = "+(a/b));
+    }
+}
